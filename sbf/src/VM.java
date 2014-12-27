50ad7c0455b24cdf0bafdd2bbd866e757af7e130
jake100 <jake_hall_programming@outlook.com>
2014-12-26 23:22:30 -0800

    1st commit

---
+++ sbf/src/VM.java
@@ -0,0 +1,117 @@
+import java.util.*;
+public class VM {
+    private Scanner sc = new Scanner(System.in);
+    private final int LENGTH = 65535;
+    private byte[] mem = new byte[LENGTH];
+    private int dataPointer;
+    public Stack<Integer> stack = new Stack<Integer>();
+    public void interpret() {
+    	evaluate(sc.nextLine());
+    }
+    public void interpret(String code) {
+       evaluate(code);
+    }
+    private void evaluate(String code)
+    {
+    	 int l = 0;
+         for(int i = 0; i < code.length(); i++) {
+         	String num = "";
+         	if((""+code.charAt(i)).matches("\\d")) {
+         		num += code.charAt(i);
+                 for(i = 0; i < code.length(); i++) {
+         			if((""+code.charAt(i)).matches("\\d"))num += code.charAt(i);
+         		}
+         		stack.push(Integer.parseInt(num));
+         		i--;
+         		
+         	} else if(code.charAt(i) == '>') {
+                 dataPointer = (dataPointer == LENGTH-1) ? 0 : dataPointer + 1;
+             } else if(code.charAt(i) == '<') {
+                 dataPointer = (dataPointer == 0) ? LENGTH-1 : dataPointer - 1;
+             } else if(code.charAt(i) == '+') {
+                 mem[dataPointer]++;
+             } else if(code.charAt(i) == '-') {
+                 mem[dataPointer]--;
+             } else if(code.charAt(i) == '.') {
+                 System.out.print((char) mem[dataPointer]);
+             } else if(code.charAt(i) == ',') {
+                 mem[dataPointer] = (byte) sc.next().charAt(0);
+             } else if(code.charAt(i) == ':'){
+             	stack.push((int) mem[dataPointer]);
+             } else if(code.charAt(i) == ';'){
+             	mem[dataPointer] = (byte) stack.pop().intValue();
+             } else if(code.charAt(i) == '_'){
+             	stack.pop();
+             } else if(code.charAt(i) == '='){
+             	stack.push(stack.peek());
+             } else if(code.charAt(i) == '@') {
+            	 stack.push(dataPointer);
+             } else if(code.charAt(i) == '*') {
+            	 dataPointer = stack.pop();
+             } else if(code.charAt(i) == '!') {
+            	 if(mem[dataPointer] == 0)
+            	 {
+            		 mem[dataPointer] = 1;
+            	 }
+            	 else
+            	 {
+            		 mem[dataPointer] = 0;
+            	 }
+             } else if(code.charAt(i) == '&') {
+            	 if(mem[dataPointer] == 0 && stack.pop() == 0)
+            	 {
+            		 mem[dataPointer] = 1;
+            	 }
+            	 else
+            	 {
+            		 mem[dataPointer] = 0;
+            	 }
+             } else if(code.charAt(i) == '|') {
+            	 if(mem[dataPointer] == 0 || stack.pop() == 0)
+            	 {
+            		 mem[dataPointer] = 1;
+            	 }
+            	 else
+            	 {
+            		 mem[dataPointer] = 0;
+            	 }
+             } else if(code.charAt(i) == '^') {
+            	 mem[dataPointer] = (byte) (mem[dataPointer] * 2);
+             } else if(code.charAt(i) == '/') {
+            	 mem[dataPointer] = (byte) (mem[dataPointer] / 2);
+             } else if(code.charAt(i) == '#') {
+            	 stack.push(stack.size());
+             } else if(code.charAt(i) == '~') {
+            	 stack.push(new Random().nextInt(1));
+             } else if(code.charAt(i) == '\"') {
+            	 mem[dataPointer] += stack.pop();
+             } else if(code.charAt(i) == '\'') {
+            	 mem[dataPointer] -= stack.pop();
+             } else if(code.charAt(i) == '[') {
+            	 
+                 if(mem[dataPointer] == 0) {
+                     i++;
+                     while(l > 0 || code.charAt(i) != ']') {
+                         if(code.charAt(i) == '[') l++;
+                         if(code.charAt(i) == ']') l--;
+                         i++;
+                     }
+                 }
+             } else if(code.charAt(i) == ']') {
+                 if(mem[dataPointer] != 0) {
+                     i--;
+                     while(l > 0 || code.charAt(i) != '[') {
+                         if(code.charAt(i) == ']') l++;
+                         if(code.charAt(i) == '[') l--;
+                         i--;
+                     }
+                     i--;
+                 }
+             }
+         }
+    }
+    public static void main(String[] args) {
+        new VM().interpret("++^^^^^++.");
+        
+    }
+}
